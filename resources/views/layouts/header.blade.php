a4f9171de7a7a99055b43ea02a41e0116afd8ab3
bhupinder <bhupindersingh75890@gmail.com>
2021-07-04 07:51:28 +0530

    ff

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -79,7 +79,7 @@
 								<a class="nav-link" href="{{URL::to('/profile')}}">{{ Auth::user()->firstName }}</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Loogut</a>
+								<a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
 								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 									@csrf
 								</form>
