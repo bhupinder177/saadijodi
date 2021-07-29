776ec1cce5ae4d9298e91b485ee3865250ee2ca5
bhupinder <bhupindersingh75890@gmail.com>
2021-07-29 22:00:26 +0530

    dd

--- resources/views/front/contact/contact.blade.php
+++ resources/views/front/contact/contact.blade.php
@@ -1,7 +1,7 @@
 @include('layouts.header')
-<section class="map_wrapp">
+<!-- <section class="map_wrapp">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109782.69919949504!2d76.6273400302204!3d30.698452818605396!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fee906da6f81f%3A0x512998f16ce508d8!2sSahibzada%20Ajit%20Singh%20Nagar%2C%20Punjab!5e0!3m2!1sen!2sin!4v1626430473464!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-</section>
+</section> -->
 
 <section class="contact_sec">
   <div class="container">
