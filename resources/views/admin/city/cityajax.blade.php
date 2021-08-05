30bc967f2cd56f95eff41ff49f8607a9f68d67e1
bhupinder <bhupindersingh75890@gmail.com>
2021-08-05 23:38:30 +0530

    ss

--- resources/views/admin/city/cityajax.blade.php
+++ resources/views/admin/city/cityajax.blade.php
@@ -2,9 +2,8 @@
     <thead>
         <tr>
             <th style="width: 5%">S. No</th>
-            <th style="width: 10%">State</th>
-            <th style="width: 10%">Timezone</th>
-            <th style="width: 8%">City</th>
+            <th style="width: 10%">City</th>
+
             <th style="width: 10%">Actions</th>
         </tr>
     </thead>
@@ -16,8 +15,6 @@
         <tr class="data">
             <td>{{ ++$srNo }}</td>
             <td>{{ $u->name }}</td>
-                <td>{{ $u->timezone }}</td>
-<td><a href="{{ url($prefix.'/city/'.$u->id)}}" class="btn btn-success">Add City</a></td>
             <td >
               <a href="{{url($prefix.'/state-edit/'.Crypt::encrypt($u->id))}}"><i class="fa fa-edit"></i></a>
               <a class="deleterecord" data-link="{{url($prefix.'/state-delete')}}" data-id="{{ $u->id }}"><i class="fa fa-trash deleterecord"  data-link="{{url($prefix.'/state-delete')}}"  data-id="{{ $u->id }}" aria-hidden="true"></i></a>
