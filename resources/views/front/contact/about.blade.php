a9cb589d89de9b49cdb476f0411b8a283140dc97
bhupinder <bhupindersingh75890@gmail.com>
2021-08-05 23:17:00 +0530

    ss

--- resources/views/front/contact/about.blade.php
+++ resources/views/front/contact/about.blade.php
@@ -10,8 +10,20 @@
       <div class="col-md-12">
         <p>SaadiJodi.com is the pioneer online matchmaking site. It came into existence intending to make you feel the love of your partner. Now Indians can get the perfect matchmaking experience all across the world.  </p>
         <p>In this modern era, matrimonial sites have become a convenient way to connect people on one single platform. With such availability, youth and even though 40 plus adults have the opportunity to swipe right to their favourite profile. A user can find the suitors by searching for some required details like age, gender, religion, location, caste, and nationality.  </p>
-        <p>With matrimonial sites, the bride and groom have a chance to go with their preferred profile or the parents' choice. After connecting with the right partner, it's more appropriate to meet them and share some quality time outside this platform. In reality, it’s a great gift for users who’re annoying with the custom of going outside with parents and meet with a girl/boy for a marriage ceremony. We give you an amazing option to click the profile you like the most and at some point in time, if you find the profile’s individual is not compatible, you can step out with another person. </p>
+        <p>In reality, it’s a great gift for users who’re annoying with the custom of going outside with parents and meet with a girl/boy for a marriage ceremony. </p>
         <p>Through an online chat service, you can communicate with match and this will help you to know more about each other. At last, both can decide for a wedding. </p>
+        <p><b>Find your special match</b></p>
+        <p><b>Sign Up:</b> Create your profile & add your details</p>
+        <p><b>Send Invitation:</b> Select the profile & forward your proposal</p>
+        <p><b>Interaction:</b> Be a premium member and start your Chat process</p>
+        <p>With matrimonial sites, the bride and groom have a chance to go with their preferred profile or the parents' choice. </p>
+        <p>After connecting with the right partner, it's more appropriate to meet them and share some quality time outside this platform.</p>
+        <p>We give you an amazing option to click the profile you like the most and at some point in time, if you find the profile’s individual is not compatible, you can step out with another person. </p>
+        <p>Through an online chat service, you can communicate with match and this will help you to know more about each other. At last, both can decide for a wedding. </p>
+        <p><b>Why SaadiJodi.com is the right choice?</b></p>
+        <p><b>Privacy Policy:</b> Control the unacceptable access to photos and personal details</p>
+        <p><b>100% verified profiles:</b> Personal visit by our special agents to avoid fake profiles</p>
+        <p><b>Classified:</b> Each profile is categorized by religion, community, and language</p>
         <p>You’re not late yet!!! Still have the time to start another chapter of your life!!</p>
         <p>SaadiJodi.com, is a reliable destination to offer magnificent matchmaking links exploring the most genuine resources to meet true potential partners. By keeping in mind the objective of our site, we successfully touch the heart of millions of people worldwide.</p>
         <p><b>Benefits of using the SaadiJodi.com matrimonial website:</b></p>
@@ -19,7 +31,8 @@
         <p><b>Save quality time and income:</b> Trust us, we’re here to save your precious time and money. Whenever get time, explore different profiles on your portable gadgets, if you like any, message instantly. In short, a matrimonial site is much cheaper as there is no longer pay for a priest or a marriage broker for match-making.</p>
         <p><b>Vast Connection:</b> Through matrimonial sites, you can connect to different communities and castes overseas. There are no geographical boundaries, a widespread search can happen with a couple of minutes.</p>
         <p><b>Preference results:</b> Search the caste, location, and profession according to your preference. Filters help to make you reach your soulmate. </p>
-        <p><b>Safety:</b> All the registered profiles are purely safe and secure on our site. A user can maintain their confidentiality with us. You can make your profile private and can share it with only selected people. </p>
+        <p><b>Safety:</b> All the registered profiles are purely safe and secure on our site. A user can maintain their confidentiality with us. You can make your profile private and can share it with only selected people.</p>
+        <p><b>In true words, SaadiJodi.com is a one-stop destination to tie a knot with an ideal match.</b></p>
       </div>
     </div>
 
